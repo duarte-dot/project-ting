1059ea46cc9169ad5070fbe23f236b10858898f6
duarte <gabrieldvr@outlook.com>
2023-08-06 14:20:02 -0300

    fila para armazenar os arquivos que serão lidos.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,4 +1,2 @@
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
-
-# iniciando projeto
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,26 @@
-from ting_file_management.abstract_queue import AbstractQueue
-
-
-class Queue(AbstractQueue):
+class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.queue = []
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        if not self.queue:
+            raise IndexError(
+                "A fila está vazia, não é possível remover elementos."
+                )
+        return self.queue.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        if not (0 <= index < len(self.queue)):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.queue[index]
