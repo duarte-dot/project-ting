cdb032adb2baf911d01ba328596e282d31f9c5e9
duarte <gabrieldvr@outlook.com>
2023-08-06 14:22:18 -0300

    transformar o conteúdo da lista gerada pela função txt_importer em um dicionário que será armazenado dentro da Queue

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write("Formato inválido\n")
+        return []
+
+    try:
+        with open(path_file, 'r') as file:
+            lines = file.read().split('\n')
+            return lines
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return []
